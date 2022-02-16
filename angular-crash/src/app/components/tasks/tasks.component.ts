c18e5f1042e2430823f8e424c4f840255764a8dd
Sein Izumita <sein.izumita@gmail.com>
2022-02-16 18:11:23 -0500

    adding tasks

---
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import { TASKS } from 'src/app/mock-tasks';
+import { Task } from 'src/app/Task';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css']
+})
+export class TasksComponent implements OnInit {
+  tasks: Task[] = TASKS;
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ angular-crash/src/app/mock-tasks.ts
@@ -0,0 +1,23 @@
+import { Task } from "./Task"
+
+export const TASKS: Task[] = [
+    {
+        id:1,
+        text: 'Cold Start',
+        day: 'Feb 21st at 8:00',
+        reminder: true
+    },
+    {
+        id:2,
+        text: 'Coding practice',
+        day: 'Feb 21st at 11:00',
+        reminder: true
+    },
+    {
+        id:3,
+        text: 'Elden Ring',
+        day: 'Feb 21st at 16:00',
+        reminder: true
+    },
+    
+]
