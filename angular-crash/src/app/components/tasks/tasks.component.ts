e688b92a40d8eba5a341a77405bdc7598ae0ee08
Sein Izumita <sein.izumita@gmail.com>
2022-02-17 15:36:55 -0500

    made tasks use observables and created mock json server

--- angular-crash/src/app/components/tasks/tasks.component.ts
+++ angular-crash/src/app/components/tasks/tasks.component.ts
@@ -5,15 +5,15 @@ import { Task } from 'src/app/Task';
 @Component({
   selector: 'app-tasks',
   templateUrl: './tasks.component.html',
-  styleUrls: ['./tasks.component.css']
+  styleUrls: ['./tasks.component.css'],
 })
 export class TasksComponent implements OnInit {
   tasks: Task[] = [];
 
-  constructor(private taskService: TaskService) { }
+  constructor(private taskService: TaskService) {}
 
   ngOnInit(): void {
-    this.tasks = this.taskService.getTasks();
+    this.taskService.getTasks().subscribe((tasks) => (this.tasks = tasks));
   }
 
 }
--- angular-crash/src/app/sevices/task.service.ts
+++ angular-crash/src/app/sevices/task.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { Observable, of} from 'rxjs';
 import { Task } from '../Task';
 import { TASKS } from '../mock-tasks';
 
@@ -9,7 +10,8 @@ export class TaskService {
 
   constructor() { }
 
-  getTasks(): Task[]{
-    return TASKS;
+  getTasks(): Observable <Task[]>{
+    const tasks = of(TASKS);
+    return tasks;
   }
 }
