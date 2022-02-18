fff5b06b567fa3dbcdf9bd49a91ee9a9000db3af
Sein Izumita <sein.izumita@gmail.com>
2022-02-18 13:51:32 -0500

    setup to fetch data from the backend

--- angular-crash/src/app/sevices/task.service.ts
+++ angular-crash/src/app/sevices/task.service.ts
@@ -1,18 +1,17 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpHeaders} from '@angular/common/http';
-import { Observable, of} from 'rxjs';
+import { Observable} from 'rxjs';
 import { Task } from '../Task';
-import { TASKS } from '../mock-tasks';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = 'http://localhost:5000/tasks'
 
-  constructor() { }
+  constructor(private http: HttpClient) { } //able to fetch tasks 
 
   getTasks(): Observable <Task[]>{
-    const tasks = of(TASKS);
-    return tasks;
+    return this.http.get<Task[]>(this.apiUrl) //makes it so the data is being feteched from the backend rather than being imported from a file
   }
 }
